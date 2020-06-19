4e1ba0ae0e080f547dff509c6acef236502ea115
Cathella <ncathie214@gmail.com>
2020-06-19 22:44:14 +0300

    Add flash message to actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
@@ -24,11 +26,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to action: "index"
   end
 
   def edit
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 BlogApp::Application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
